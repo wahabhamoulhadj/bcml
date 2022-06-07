8998c08c496b9f2e33c90e26c13868b07bc20f2a
GVS-007 <gundavenkatasai53@gmail.com>
2022-06-07 18:52:08 -0400

    New_code. Written for cross validation.

--- PROMIS/code.py
+++ PROMIS/code.py
@@ -24,6 +24,57 @@ for fname in glob.glob(path):
     scaler = MinMaxScaler()
     X = scaler.fit_transform(X)
     X_train, X_test, Y_train, Y_test = train_test_split( X, Y, test_size = 3/10, random_state = 43 )
-    folds = StratifiedKFold(n_splits= 5)
+    cv = StratifiedKFold(n_splits=5)
+    classifier = LogisticRegression()
+    tprs = []
+    aucs = []
+    mean_fpr = np.linspace(0, 1, 100)
+    fig, ax = plt.subplots()
+    for i, (train, test) in enumerate(cv.split(X_train, Y_train)):
+        classifier.fit(X_train[train], Y_train[train])
+        viz = RocCurveDisplay.from_estimator(
+            classifier,
+            X_train[train],
+            Y_train[train],
+            name="ROC fold {}".format(i), alpha= 3/10, lw=1,
+            ax=ax,
+        )
+        interp_tpr = np.interp(mean_fpr, viz.fpr, viz.tpr)
+        interp_tpr[0] = 0.0
+        tprs.append(interp_tpr)
+        aucs.append(viz.roc_auc)
+    ax.plot([0, 1], [0, 1], linestyle="--", lw=2, color="r", label="Chance", alpha=0.8)
 
-    print(cross_val_score(LogisticRegression(), X_train, Y_train, cv=5))
+    mean_tpr = np.mean(tprs, axis=0)
+    mean_tpr[-1] = 1.0
+    mean_auc = auc(mean_fpr, mean_tpr)
+    std_auc = np.std(aucs)
+    ax.plot(
+        mean_fpr,
+        mean_tpr,
+        color="b",
+        label=r"Mean ROC (AUC = %0.2f $\pm$ %0.2f)" % (mean_auc, std_auc),
+        lw=2,
+        alpha=0.8,
+    )
+
+    std_tpr = np.std(tprs, axis=0)
+    tprs_upper = np.minimum(mean_tpr + std_tpr, 1)
+    tprs_lower = np.maximum(mean_tpr - std_tpr, 0)
+    ax.fill_between(
+        mean_fpr,
+        tprs_lower,
+        tprs_upper,
+        color="grey",
+        alpha=0.2,
+        label=r"$\pm$ 1 std. dev.",
+    )
+
+    ax.set(
+        xlim=[-0.05, 1.05],
+        ylim=[-0.05, 1.05],
+        title="Receiver operating characteristic example",
+    )
+    ax.legend(loc="lower right")
+    plt.show()
+    print(cross_val_score(classifier, X_train, Y_train, cv=5))
