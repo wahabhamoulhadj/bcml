120e6331bc322995b3c4e81f647eee6753564445
GVS-007 <gundavenkatasai53@gmail.com>
2022-06-22 20:50:10 -0400

    New_code. Written for cross validation.

--- Algorithms/combine.py
+++ Algorithms/combine.py
@@ -1,4 +1,8 @@
 import pandas as pd
-df = pd.concat([pd.read_csv('../Tables/AUC_Table.csv'), pd.read_csv('AUC_Table_BBC.csv')], axis=1, join='outer' )
+df = pd.read_csv('../Tables/AUC_Table.csv')
+l = pd.Series(['project_' + str(i+1) for i in range(27)])
+df = pd.concat([l , df], axis=1, join='outer' )
 df.drop(df.filter(regex="Unnamed"),axis=1, inplace=True)
+df.set_index(0)
+
 df.to_csv('../Tables/AUC_Table.csv', index=False)
